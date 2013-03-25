8c80f67ce00f1abca635a690445b85f328b7fd96
Brian Hatfield <bhatfield@brightcove.com>
2013-03-25 19:27:39 -0400

    Actually make event and task classes do something

--- lib/event.py
+++ lib/event.py
@@ -1,21 +1,34 @@
-# Riemann client library, depends on protobufs
-# https://github.com/banjiewen/bernhard
-import bernhard
-
 # For determining hostname
 import socket
 
-class Event():
-	def __init__(self, service, state, description, ttl, metric=None, hostname=socket.gethostname()):
-		self.event = {}
-		self.event['service'] = service
-		self.event['state'] = state
-		self.event['description'] = description
-		self.event['ttl'] = ttl
-		self.event['hostname'] = hostname
+import logging
+log = logging.getLogger(__name__)
+
+class Events():
+	def __init__(self):
+		self.events = []
+
+	def add(self, service, state, description, ttl, tags=None, metric=None, hostname=socket.gethostname()):
+		event = {}
+		event['service'] = service
+		event['state'] = state
+		event['description'] = description
+		event['ttl'] = ttl
+		event['hostname'] = hostname
 		
+		if tags is not None:
+			event['tags'] = tags
+
 		if metric is not None:
-			self.event['metric'] = metric
+			event['metric'] = metric
+
+		log.debug("Event added: %s" % (event))
+		self.events.append(event)
 
 	def send(self, client):
-		client.send(self.event)
+		log.debug("Sending %s events..." % (len(self.events)))
+		while len(self.events) > 0:
+			event = self.events.pop(0)
+			client.send(event)
+
+	
--- lib/task.py
+++ lib/task.py
@@ -1,22 +1,72 @@
-# Subprocess
-import subprocess
+import logging
+log = logging.getLogger(__name__)
 
-# For splitting command lines into popen forms
-# http://docs.python.org/2/library/subprocess.html#subprocess.Popen
+# Events, for Task
+import event
+
+# Subprocess, for NagiosTask
+import subprocess
 import shlex
 
-class Task():
-	def __init__(self, command, ttl, shell=False):
-		self.command = shlex.split(command)
-		self.ttl = ttl
-		self.use_shell = shell
+# Multiprocessing, for PythonTask
+import multiprocessing
 
-	def run(self):
-		self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, shell=self.use_shell)
+class Task():
+	def __init__(self, name, ttl):
+		self.events = event.Events()
+		self.name = name
+		self.ttl = ttl
+		self.tags = []
 
 	def start(self):
 		self.run()
 
-	def result(self):
+	def get_events(self):
+		self.join()
+
+		return self.events
+
+
+class PythonTask(Task):
+	def __init__(self, name, ttl, module):
+		Task.__init__(self, name, ttl)
+		self.module = module
+
+	def run(self):
+		pass
+
+	def join(self):
+		self.events.add(service=self.name, state=state, description=description, ttl=self.ttl, tags=self.tags)
+
+
+class NagiosTask(Task):
+	exitcodes = {
+		0: 'ok',
+		1: 'warn',
+		2: 'error',
+		3: 'unknown'
+	}
+
+	def __init__(self, name, ttl, command, shell=False):
+		Task.__init__(self, name, ttl)
+		self.raw_command = command
+		self.command = shlex.split(command)
+		self.use_shell = shell
+
+	def run(self):
+		try:
+			self.process = subprocess.Popen(self.command, stdout=subprocess.PIPE, shell=self.use_shell)
+		except Exception as e:
+			log.error("Exception running task '%s':\n%s" % (self.name, str(e)))
+
+	def join(self):
 		stdout, sterr = self.process.communicate()
-		return (stdout, stderr, self.process.returncode)
+		description = self.raw_command + "\n" + stdout
+		returncode = self.process.returncode
+
+		if returncode in self.exitcodes:
+			state = self.exitcodes[returncode]
+		else:
+			state = 'unknown'
+
+		self.events.add(service=self.name, state=state, description=description, ttl=self.ttl, tags=self.tags)
