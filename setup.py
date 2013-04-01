d51445b5dc8b33de1f3ab4701ac2809b9997fe1d
Brian Hatfield <bhatfield@brightcove.com>
2013-04-01 14:14:10 -0400

    Modify path to sumd.conf

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name="riemann-sumd",
       package_dir={'': 'lib'},
       py_modules=['event', 'loader', 'scheduler', 'sender', 'task'],
       data_files=[('/etc/init/', ["init/ubuntu/sumd.conf"]),
-                  ('/etc/sumd', ['examples/etc/sumd/sumd.conf.example']),
+                  ('/etc/sumd', ['examples/etc/sumd/sumd.conf']),
                   ('/etc/sumd/tasks.d', ['examples/etc/sumd/tasks.d/simple.task.example']),
                   ('/etc/sumd/tags.d', ['examples/etc/sumd/tags.d/simple.tag.example'])],
       scripts=["bin/sumd"]
