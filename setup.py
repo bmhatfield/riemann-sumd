01008c2ce945477379ffeea3babe1c1472fe61ef
Brian Hatfield <bhatfield@brightcove.com>
2013-04-01 13:49:37 -0400

    Update examples and gitignore to support updated packaging configs

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ import platform
 
 version = "0.1.0"
 
-setup(name="chef-registration",
+setup(name="riemann-sumd",
       version=version,
       description="Python agent for scheduling event generating processes and sending the results to Riemann",
       author="Brian Hatfield",
       author_email="bmhatfield@gmail.com",
       url="https://github.com/bmhatfield/riemann-sumd",
-      data_files=[('/etc/init/', ["init/sumd.conf"])],
+      package_dir={'': 'lib'},
+      py_modules=['event', 'loader', 'scheduler', 'sender', 'task'],
+      data_files=[('/etc/init/', ["init/ubuntu/sumd.conf"]),
+                  ('/etc/sumd', ['examples/etc/sumd/sumd.conf.example']),
+                  ('/etc/sumd/tasks.d', ['examples/etc/sumd/tasks.d/simple.task.example']),
+                  ('/etc/sumd/tags.d', ['examples/etc/sumd/tasks.d/simple.tag.example'])],
       scripts=["bin/sumd"]
     )
